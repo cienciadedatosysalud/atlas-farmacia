778d9e0a8bf532fadd5c6fb62998b425eb2fb055
Javier Gonzalez <jgonzalezga.iacs@gmail.com>
2023-07-27 13:18:17 +0200

    initial commit

---
+++ atlas_antibioticos/README.md
@@ -0,0 +1,113 @@
+![Logo of the project](https://cienciadedatosysalud.org/wp-content/uploads/logo-Data-Science-VPM.png)
+
+<small><i>This project follows the structure built using the [Common Data Model Builder](https://github.com/cienciadedatosysalud/cdmb), a tool that allows you to create common data models to facilitate interoperability and reproducibility of the analyses.</i></small>
+
+
+# ATLAS FARMACIA - ANTIBIÓTICOS
+
+---
+
+# Outputs
+Outputs structure and content is described below including the files and folders that are generated when creating a research project with the `cdmb` Python library. There are four main folders corresponding to:
+
+- __docs/CDM/__
+  - **cdmb_config.json**: Configuration file.
+  - **cohort_definition_inclusion.csv**: csv file that defines the criteria (i.e., codes) for inclusion in a cohort.
+  - **cohort_definition_exclusion.csv**: csv file that defines the criteria (i.e., codes) for exclusion in a cohort.
+  - **common_datamodel.xlsx**: The definition of the common data model in Excel format.
+  - **entities/**: Folder structure where, for each defined entity, the catalogs and the established validation rules are stored.
+  - **ER.gv, ER.gv.png**: an Entity-Relationship Diagram of the entities included in the CDM.
+  - **synthetic-data/**: Folder structure contaning an automatically generated set of 1000 synthetic records per entity included en the CDM.
+  - **hashed_files_list.json**: List of the files generated or used after generating the project with their md5 hash. This file must be kept hidden 
+and should be used to cross-check with the results obtained from the analysis from the original input files.
+- __inputs/__
+  - **data.duckdb**: Database that temporarily contains the data entered by the user (synthetic data by default)
+- __outputs/__
+  - (Default directory of all the outputs produced in the project execution)
+- __src/__
+  - __analysis-scripts/__
+    - (directory where the analysis scripts developed by the user are stored)
+    - **_quarto.yml**: File containing the Metadata to execute Quarto documents.
+  - __check_load-scripts/__
+    - **check_load.py**: Script in charge of the mapping between the files introduced by the user (./inputs) and map them to the defined entities (inputs/data.duckdb). 
+    In the loading process, the following checks are performed: Name of the variables match; the format/type of the variables match those established in the configuration.
+    - __inputs/__: Auxiliary folder for the script 'check_load.py'.
+  - __dqa-scripts/__
+    - **dqa.py**: Data Quality Assesment script by default.
+  - **validation-scripts/**
+    - **validator.py**: Script in charge of applying the validation rules to the data.
+    - **valididator_report.qmd**: Quarto document that generates a report in html from the results obtained from 'validator.py'. 
+    - **_quarto.yml**: File containing metadata to execute Quarto documents.
+- **ro-crate-metadata.json**: Accessible and practical formal metadata description for use in a wider variety of situations, 
+from an individual researcher working with a folder of data, to large data-intensive computational research environments. For more information, visit [RO-Crate](https://www.researchobject.org/ro-crate/).
+- **man_container_deployment.md**: From Data Science for Health Services and Policy Research group we provide in the following
+  GitHub repository, a solution, for the deployment of the generated project. This step is optional.
+- **LICENSE.md**: Project license.
+
+
+## Requirements/Dependencies 
+
+## R dependencies
+Version of Rbase used: **4.2.3**
+
+Version of [Quarto](https://quarto.org/) used: **1.2.475**
+
+| library    | version | link                                                                                    |
+|------------|---------|-----------------------------------------------------------------------------------------|
+| DuckDB     | 0.8.1   | https://duckdb.org/                                                                     |
+| jsonlite   | 1.8.7   | https://cran.r-project.org/web/packages/jsonlite/index.html                             |
+| kableExtra | 1.3.4   | https://cran.r-project.org/web/packages/kableExtra/vignettes/awesome_table_in_html.html |
+| Hmisc      | 4.7.1   | https://cran.r-project.org/package=Hmisc                                                |
+| sf         | 1.0_14  | https://cran.r-project.org/web/packages/sf/index.html                                   |
+| sjmisc     | 2.8.9   | https://cran.r-project.org/web/packages/sjmisc/index.html                               |
+| gt         | 0.9.0   | https://cran.r-project.org/web/packages/gt/index.html                                   |
+| logger     | 0.2.2   | https://cran.r-project.org/web/packages/logger/index.html                               |
+| cowplot    | 1.1.1   | https://cran.r-project.org/web/packages/cowplot/index.html                              |
+| ggalluvial | 0.12.5  | https://cran.r-project.org/web/packages/ggalluvial/index.html                           |
+| ggalluvial | 0.9.3   | https://cran.r-project.org/web/packages/ggrepel/index.html                              |
+| ggplot2    | 3.4.2   | https://cran.r-project.org/web/packages/ggplot2/index.html                              |
+| plotly     | 4.10.2  | https://cran.r-project.org/web/packages/plotly/index.html                               |
+
+## Python dependencies
+Version of Python used: **>=3.9.2**
+
+| library         | version | link                                                    |
+|-----------------|---------|---------------------------------------------------------|
+| pandas          | 1.5.3   | https://pandas.pydata.org/                              |
+| DuckDB          | 0.8.1   | https://duckdb.org/                                     |
+| ydata_profiling | 4.1.2   | https://ydata-profiling.ydata.ai/docs/master/index.html |
+
+
+# Authoring
+
+| Surname, name | Affiliation | ![orcid](https://orcid.org/sites/default/files/images/orcid_16x16.png) ORCID |
+|---------------|-------------|------------------------------------------------------------------------------|
+| Grupo de Investigación en Ciencia de Datos para Servicios y Políticas de Salud | Instituto Aragonés de Ciencias de la Salud (IACS), Instituto de Investigación Sanitaria de Aragón (IIS) | |
+| Martínez-Lizaga, Natalia | Instituto Aragonés de Ciencias de la Salud (IACS) | https://orcid.org/0000-0002-9586-7955 |
+| Estupiñan-Romero, Francisco | Instituto Aragonés de Ciencias de la Salud (IACS) | https://orcid.org/0000-0002-6285-8120 |
+| Ridao-López, Manuel | Instituto de Investigación Sanitaria de Aragón (IIS) | https://orcid.org/0000-0001-7837-5759 |
+| González-Galindo, Javier | Instituto Aragonés de Ciencias de la Salud (IACS) | https://orcid.org/0000-0002-8783-5478 |
+| Royo-Sierra, Santiago | Instituto Aragonés de Ciencias de la Salud (IACS) | https://orcid.org/0000-0002-0048-4370 |
+| Bernal-Delgado, Enrique | Instituto Aragonés de Ciencias de la Salud (IACS) | https://orcid.org/0000-0002-0961-3298 |
+
+__Project leader: [Bernal-Delgado, Enrique](https://orcid.org/0000-0002-0961-3298)__
+
+# Previous version(s):
+A previous version (v.1.0.3) of this common data model not compliant with the structure produced by the `cdmb` Python library has been published in ZENODO at
+
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.8016643.svg)](https://doi.org/10.5281/zenodo.8016643)
+
+# How to contribute
+- Repository: https://github.com/cienciadedatosysalud/atlas-farmacia
+- Issue tracker: https://github.com/cienciadedatosysalud/atlas-farmacia/issues
+
+# References
+- Data Science for Health Services and Policy Research group: https://cienciadedatosysalud.org/en/
+- Common Data Model Builder library :https://github.com/cienciadedatosysalud/cdmb
+- Analytic Software Pipeline Interface for Reproducible Execution (ASPIRE): https://github.com/cienciadedatosysalud/ASPIRE
+- Atlas VPM community in Zenodo: https://zenodo.org/communities/atlasvpm
+- Research Object Crate (RO-Crate): https://www.researchobject.org/ro-crate/
+- ORCID: https://orcid.org/
+
+<a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" ><img src="https://img.shields.io/badge/license-CC--BY%204.0-lightgrey" alt="License: CC-BY 4.0"></a>
+
---
+++ atlas_antibioticos/src/analysis-scripts/farmacia_standardized_antibioticos4.R
@@ -0,0 +1,337 @@
+
+
+auxilary_database_path <- '../../inputs/data.duckdb'
+outputs_path <- '../../outputs/'
+
+leerPoblacionAño <- function(año,data){
+  log_info('Connect to population database')
+  ccaa_cd <- unique(data$ccaa_cd)
+  
+  con = dbConnect(duckdb::duckdb(), dbdir=auxilary_database_path, read_only=FALSE)
+  
+  
+  population <- dbGetQuery(conn = con, paste0("SELECT año_cd,ccaa_cd,zbs_residencia_cd,grupo_edad_cd,sexo_cd,sum(n_poblacion) as n_poblacion 
+                                              FROM zona_basica_tarjeta where sexo_cd IN (1,2) and ccaa_cd = '",ccaa_cd,"' and año_cd = ",año," 
+                                              GROUP BY año_cd,ccaa_cd,zbs_residencia_cd,grupo_edad_cd,sexo_cd")) %>%
+    rename(sexo = sexo_cd,
+           gqe = grupo_edad_cd)
+  comb_zbs <- population$zbs_residencia_cd %>% unique()
+  comb_gqe <- seq(1,18)
+  comb_sexo <- seq(1,2)
+  comb_total <- expand.grid(comb_zbs,comb_gqe,comb_sexo) %>%
+    rename(
+      gqe = Var2,
+      sexo = Var3,
+      zbs = Var1
+    )
+  
+  population <- population %>% select(zbs_residencia_cd,sexo,gqe,n_poblacion) %>% rename(zbs = zbs_residencia_cd)
+  
+  population <- merge(x=comb_total,y=population,all.x = TRUE)
+  population$ccaa_cd <- ccaa_cd
+  
+  dbDisconnect(con, shutdown=TRUE)
+  log_info('Disconnect to population database')
+  return(population)
+}
+
+getPoblacionRef <- function(año){
+  log_info('Calculate population ref')
+  con = dbConnect(duckdb::duckdb(), dbdir=auxilary_database_path, read_only=FALSE)
+  
+  
+  population <- dbGetQuery(conn = con, paste0("SELECT * FROM zona_basica_ine where sexo_cd IN (1,2) and año_cd = ",año)) %>%
+    rename(sexo = sexo_cd,
+           gqe = grupo_edad_cd)
+  
+  
+  poblacion_ref <- population %>%
+    group_by(sexo,gqe) %>%
+    summarise(pop = sum(n_poblacion,na.rm = TRUE))
+  poblacion_ref[poblacion_ref$pop==0,"pop"] <- 1 # !!! No puedo tener poblaciones 0
+  
+  dbDisconnect(con, shutdown=TRUE)
+  return(poblacion_ref)
+}
+
+
+leerDataAño4 <- function(año){
+  log_info(paste0('Connect to database  (',año,')'))
+  con = dbConnect(duckdb::duckdb(), dbdir=auxilary_database_path, read_only=FALSE)
+  indicador_ <- dbGetQuery(conn = con, paste0(
+    "
+with partial_df as (
+select
+	envase_id,
+	isoyear(fecha_dispensacion_dt) as año,
+	paciente_id,
+	CASE
+		WHEN edad_nm >= 0
+		and edad_nm <= 4 THEN 1
+		WHEN edad_nm >= 5
+		and edad_nm <= 9 THEN 2
+		WHEN edad_nm >= 10
+		and edad_nm <= 14 THEN 3
+		WHEN edad_nm >= 15
+		and edad_nm <= 19 THEN 4
+		WHEN edad_nm >= 20
+		and edad_nm <= 24 THEN 5
+		WHEN edad_nm >= 25
+		and edad_nm <= 29 THEN 6
+		WHEN edad_nm >= 30
+		and edad_nm <= 34 THEN 7
+		WHEN edad_nm >= 35
+		and edad_nm <= 39 THEN 8
+		WHEN edad_nm >= 40
+		and edad_nm <= 44 THEN 9
+		WHEN edad_nm >= 45
+		and edad_nm <= 49 THEN 10
+		WHEN edad_nm >= 50
+		and edad_nm <= 54 THEN 11
+		WHEN edad_nm >= 55
+		and edad_nm <= 59 THEN 12
+		WHEN edad_nm >= 60
+		and edad_nm <= 64 THEN 13
+		WHEN edad_nm >= 65
+		and edad_nm <= 69 THEN 14
+		WHEN edad_nm >= 70
+		and edad_nm <= 74 THEN 15
+		WHEN edad_nm >= 75
+		and edad_nm <= 79 THEN 16
+		WHEN edad_nm >= 80
+		and edad_nm <= 84 THEN 17
+		WHEN edad_nm >= 85 THEN 18
+		ELSE NULL
+	END as grupo_edad_cd,
+	sexo_cd,
+	tsi_cd,
+	zbs_residencia_cd,
+	ccaa_cd,
+	atc_farmaco_dispensado_cd[1:4] as atc_farmaco_dispensado_4,
+	atc_farmaco_dispensado_cd[1:5] as atc_farmaco_dispensado_5,
+	codigo_nacional_farmaco_cd,
+	ddd_nomenclator_nm,
+  ddd_por_envase,
+	pvp_nomenclator_nm,
+	precio_por_envase_nm
+from
+	main.envase_dispensado_view)
+select a.* from (select case when atc_farmaco_dispensado_5 = 'J01MA' then atc_farmaco_dispensado_5
+else atc_farmaco_dispensado_4 end as atc_farmaco_dispensado ,ccaa_cd, zbs_residencia_cd ,sexo_cd, grupo_edad_cd, tsi_cd , 
+sum(ddd_nomenclator_nm) as ddd_nomenclator_nm,
+sum(ddd_por_envase) as ddd_por_envase,
+sum(pvp_nomenclator_nm) as pvp_nomenclator_nm,
+sum(precio_por_envase_nm) as precio_por_envase_nm,
+count(DISTINCT paciente_id) as pacientes_nm,
+count(DISTINCT envase_id) as n_envases,
+from partial_df where año = ",año," and sexo_cd in (1,2) and (atc_farmaco_dispensado_4 in ('J01C','J01D','J01F','J01X') or atc_farmaco_dispensado_5 = 'J01MA') group by atc_farmaco_dispensado ,ccaa_cd, zbs_residencia_cd ,sexo_cd, grupo_edad_cd, tsi_cd) a 
+"
+  )) %>% 
+    rename(sexo = sexo_cd,
+           gqe = grupo_edad_cd,
+           tsi = tsi_cd)
+  
+  
+  indicador_ <- indicador_ %>% rename(
+    zbs = zbs_residencia_cd) 
+  
+  
+  indicador_ <- indicador_ %>%  
+    filter(!is.na(sexo)) %>% 
+    filter(!is.na(gqe))
+  indicador_[is.na(indicador_)] <- 0
+  dbDisconnect(con, shutdown=TRUE)
+  log_info('Disconnect to database')
+  return(indicador_)
+  
+}
+
+
+leer_asignar_relaciones_zbs <- function(data){
+  
+  con = dbConnect(duckdb::duckdb(), dbdir=auxilary_database_path, read_only=FALSE)
+  rel <- dbGetQuery(conn = con, paste0("SELECT * FROM zbs_residencia_codatzbs"))
+  rel <- rel %>% filter(ccaa_cd %in% unique(data$ccaa_cd)) %>% 
+    dplyr::rename(zbs = zbs_residencia_cd)
+  data <- left_join(x=data, y=rel, by=c('zbs','ccaa_cd'))
+  data <- data %>% filter(!is.na(codatzbs))
+  dbDisconnect(con, shutdown=TRUE)
+  return(data)  
+  
+}
+
+getRatesByIndicator <- function(data,population_ref,population_aux){
+  log_info(paste0('Calculate rates and statistical indicators (',variable_indicator,')'))
+  
+  data <- left_join(x=data,
+                    y=population_ref[,c("gqe","sexo","pop")],
+                    by=c("gqe","sexo"))
+  
+  
+  #####  
+  if(variable_indicator_ == 'ddd'){
+    
+    p <- data %>% group_by(codatzbs,gqe,sexo) %>% summarise(casos = (sum(casos,na.rm=TRUE)))
+    population_aux_ <- population_aux %>% group_by(codatzbs,gqe,sexo) %>% summarise(n_poblacion = (sum(n_poblacion,na.rm=TRUE)))
+    population_ref_ <- population_ref %>% ungroup() %>% mutate(total = sum(pop))
+    population_ref_ <- population_ref_ %>% mutate(st_pop = (pop/total))
+    p <- left_join(x=p, y=population_aux_, by = c('codatzbs','gqe','sexo'))
+    data_clean <- p %>% filter(n_poblacion >0) 
+    data_clean <- left_join(x=data_clean, y = population_ref_[c("gqe","sexo","st_pop")])
+    data_clean <- data_clean %>% mutate(rate = ((1000*casos)/(365*n_poblacion)))
+    rate_dsr <- data_clean %>% group_by(codatzbs) %>% summarise(te = round(sum(st_pop * rate),4),
+                                                                casos= sum(casos,na.rm=TRUE),
+                                                                n_poblacion = sum(365*n_poblacion,na.rm=TRUE))
+    
+    
+  }else{
+
+    p <- data %>% group_by(codatzbs,gqe,sexo) %>% summarise(casos = (sum(casos,na.rm=TRUE)))
+    population_aux_ <- population_aux %>% group_by(codatzbs,gqe,sexo) %>% summarise(n_poblacion = (sum(n_poblacion,na.rm=TRUE)))
+    population_ref_ <- population_ref %>% ungroup() %>% mutate(total = sum(pop))
+    population_ref_ <- population_ref_ %>% mutate(st_pop = (pop/total))
+    p <- left_join(x=p, y=population_aux_, by = c('codatzbs','gqe','sexo'))
+    data_clean <- p %>% filter(n_poblacion >0) 
+    data_clean <- left_join(x=data_clean, y = population_ref_[c("gqe","sexo","st_pop")])
+    data_clean <- data_clean %>% mutate(rate = ((casos)/(n_poblacion)))
+    rate_dsr <- data_clean %>% group_by(codatzbs) %>% summarise(te = round(sum(st_pop * rate),4),
+                                                                casos= sum(casos,na.rm=TRUE),
+                                                                n_poblacion = sum(n_poblacion,na.rm=TRUE))
+  }
+
+  
+  data_codatzbs <- data[!duplicated(data$codatzbs),] %>% dplyr::select(codatzbs,n_zbs)
+  
+  result_end <- left_join(x=rate_dsr, y = data_codatzbs, by = 'codatzbs') %>% 
+    dplyr::rename(population = n_poblacion)
+  
+  return(result_end)
+}
+
+getRates <- function(indicator,data,patient_variables,population,population_ref,variable_indicator){
+  data <- data %>% filter(atc_farmaco_dispensado == indicator)
+  params <- c(patient_variables,variable_indicator)
+  data <- data %>% dplyr::select(all_of(params)) %>% rename(casos = variable_indicator)
+  population_aux <- population %>% 
+    filter(zbs %in% data$zbs) %>% 
+    select(zbs,sexo,gqe,n_poblacion,codatzbs,n_zbs)
+  
+  data<-merge(x=population_aux,y=data,all.x = TRUE)
+  data[is.na(data)] <- 0
+  rates <- getRatesByIndicator(data,population_ref,population_aux)
+  rates$indicator <- indicator
+  return(rates)
+}
+
+writeResults <- function(indicators_rates, estadisticosobs,year,variable_indicator_name){
+  write.csv(indicators_rates, file = paste0(outputs_path,"antibioticos4_indicators_rates_",as.character(variable_indicator_name),".csv"), 
+            row.names = FALSE)
+  write.csv(estadisticosobs, file = paste0(outputs_path,"antibioticos4_estadisticosobs_",as.character(variable_indicator_name),".csv"), 
+            row.names = FALSE)
+}
+
+
+writeResults2 <- function(indicators_rates, indicators_list,variable_indicator){
+  indicator_name <- indicators_list[1]
+  values <- indicators_rates[indicators_rates$indicator == indicator_name,
+                             c("codatzbs","population","casos","te","n_zbs")]
+  
+  
+  objeto <- list(indicator_name = values)
+  
+  names(objeto) <- c(indicator_name)
+  
+  for(i in 2:length(indicators_list)){
+    indicator_name <- indicators_list[i]
+    values <- indicators_rates[indicators_rates$indicator == indicators_list[i],c("codatzbs","population","casos","te","n_zbs")]
+    objeto[[i]] <- values
+    
+  }
+  names(objeto) <- indicators_list
+  
+  estadisticosobs<-array(0,c(length(objeto),7))
+  c.rr<-array(0,c(length(objeto),dim(objeto[[1]])[1]))
+  c.rreb<-array(0,c(length(objeto),dim(objeto[[1]])[1]))
+  for(i in 1:length(objeto)){
+    
+    attach(objeto[[i]])
+    J<-length(te)
+    
+    # 1.- razon de variacion
+    rv<-max(te)/min(te)
+    rv95<-max(te[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)])/min(te[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)])
+    rv75<-max(te[te>quantile(te, 0.25, na.rm = TRUE)& te<quantile(te, 0.75, na.rm = TRUE)])/min(te[te>quantile(te, 0.25, na.rm = TRUE)& te<quantile(te, 0.75, na.rm = TRUE)])
+    
+    #2.- coeficiente de variacion 
+    
+    cv<-sd(te)/mean(te)
+    cv95<-sd(te[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)])/mean(te[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)]) 
+    
+    # coeficiente de variacion ponderado
+    
+    cvp<-sqrt(sum(((te-mean(te))^2)*population)/(sum(population)-1))/sum(te*population/sum(population))
+    cvp95<-sqrt(sum(((te[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)]-mean(te[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)]))^2)*population[te>quantile(te, 0.05,na.rm = TRUE)& te<quantile(te, 0.95,na.rm = TRUE)])/(sum(population[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95,na.rm = TRUE)])-1))/sum(te[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)]*population[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)]/sum(population[te>quantile(te, 0.05, na.rm = TRUE)& te<quantile(te, 0.95, na.rm = TRUE)]))
+    
+    estadisticosobs[i,1:7]<-round(cbind(rv, rv95,rv75, cv, cv95, cvp, cvp95),3)
+    detach(2)
+  }
+  
+  
+  estadisticosobs<-data.frame(estadisticosobs, row.names=names(objeto))
+  names(estadisticosobs)<-c("rv", "rv95", "rv75", "cv", "cv95", "cvp", "cvp95")
+  t(round(estadisticosobs,2))
+  
+  
+  
+  indicators_rates$año <- año
+  indicators_rates <- indicators_rates %>% rename(total = casos)
+  indicators_rates_total <<- rbind(indicators_rates_total,indicators_rates)
+  estadisticosobs <- tibble::rownames_to_column(estadisticosobs, paste0("indicator"))
+  estadisticosobs$año <- año
+  estadisticosobs_total <<- rbind(estadisticosobs_total,estadisticosobs)
+  
+
+  
+}
+######################
+
+#########################
+#       PROGRAMA       #
+#########################
+for(variable_indicator in c('ddd_por_envase','pvp_nomenclator_nm')){
+  lista_años <- seq(2013,2022,1)
+  indicators_rates_total <- data.frame()
+  estadisticosobs_total  <- data.frame()
+  variable_indicator_ <- unlist(strsplit(variable_indicator,'_'))[1]
+  for(año in lista_años){
+    data <- leerDataAño4(año)
+    if(nrow(data != 0)){
+      pop <- leerPoblacionAño(año,data)
+      data <- leer_asignar_relaciones_zbs(data)
+      pop <- leer_asignar_relaciones_zbs(pop)
+      pop$n_poblacion[is.na(pop$n_poblacion)] <- 0
+      population_ref <- getPoblacionRef(año)
+    patient_variables <- c("zbs","gqe","sexo","tsi","codatzbs","n_zbs")
+    indicators_list <- c('J01C','J01D','J01F','J01MA','J01X')
+    
+    
+    indicators_rates <- lapply(indicators_list, getRates,
+                               data=data,
+                               patient_variables=patient_variables,
+                               population=pop,
+                               population_ref=population_ref,
+                               variable_indicator=variable_indicator) %>% bind_rows()
+    
+    writeResults2(indicators_rates,indicators_list, variable_indicator_)
+    
+    }
+  }
+  write.csv(indicators_rates_total, file = paste0(outputs_path,"antibioticos4_indicators_rates_",as.character(variable_indicator_),".csv"), 
+            row.names = FALSE)
+  write.csv(estadisticosobs_total, file = paste0(outputs_path,"antibioticos4_estadisticosobs_",as.character(variable_indicator_),".csv"), 
+            row.names = FALSE)
+}
+
+
+
+
